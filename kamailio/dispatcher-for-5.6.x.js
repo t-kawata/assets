446b9d8ed0520a200b368f532896fa3b303e9c6e
Toshimi Kawata <kawata@shyme.net>
2022-09-28 17:06:41 +0900

    tmp

--- kamailio/dispatcher-for-5.6.x.js
+++ kamailio/dispatcher-for-5.6.x.js
@@ -1,8 +1,31 @@
 const FLT_ACC = 1
 const FLT_ACCMISSED = 2
 const FLT_ACCFAILED = 3
-const info = function (msg) { KSR.info(msg) }
 
+/**
+ * Utils
+ */
+const info = function (msg) { KSR.info(msg) }
+const notice = function (msg) { KSR.notice(msg) }
+const getPv = function (name) { return KSR.pv.get(`$${name}`) }
+const slSendReply = function (code, reason) { return KSR.sl.sl_send_reply(code, reason) }
+
+/**
+ * Branch Routes
+ */
+const ksrRouteReqInit = function () {
+  if (!KSR.maxfwd.process_maxfwd(10)) { slSendReply(483, 'Too Many Hops'); return false; }
+  const srcIp = getPv('si')
+  const srcPort = getPv('sp')
+  info(`srcIp: ${srcIp}`)
+  info(`srcPort: ${srcPort}`)
+  if (!KSR.sanity.sanity_check(1511, 7)) { notice(`Malformed SIP message from ${srcIp}:${srcPort}`); return false }
+  return true
+}
+
+/**
+ * Request Entry Point
+ */
 function ksr_request_route() {
-  info("Hello! Now jsdt ksr_request_route is running!")
+  if (!ksrRouteReqInit()) return
 }
