177061b381f98a29a104c958a49220f073a71475
Toshimi Kawata <kawata@shyme.net>
2022-09-28 18:51:49 +0900

    tmp

--- kamailio/dispatcher-for-5.6.x.js
+++ kamailio/dispatcher-for-5.6.x.js
@@ -9,11 +9,14 @@ const info = function (msg) { KSR.info(msg) }
 const notice = function (msg) { KSR.notice(msg) }
 const getPv = function (name) { return KSR.pv.get('$' + name) }
 const slSendReply = function (code, reason) { return KSR.sl.sl_send_reply(code, reason) }
+const tCheckTrans = function () { return KSR.tm.t_check_trans() }
+const tRelay = function () { return KSR.tm.t_relay() }
+const slReplyError = function () { return KSR.sl.sl_reply_error() }
 
-/**
- * Branch Routes
- */
-const ksrRouteReqInit = function () {
+/********************************
+ * Branch Routes bgn
+ ********************************/
+const routeReqInit = function () {
   if (!KSR.maxfwd.process_maxfwd(10)) { slSendReply(483, 'Too Many Hops'); return false; }
   if (!KSR.sanity.sanity_check(1511, 7)) {
     const srcIp = getPv('si')
@@ -23,10 +26,23 @@ const ksrRouteReqInit = function () {
   }
   return true
 }
+const routeCancel = function () {
+  if (!KSR.is_CANCEL()) return true
+  if (tCheckTrans() > 0) return routeRelay()
+  return true
+}
+const routeRelay = function () {
+  if (KSR.tm.t_relay() < 0) slReplyError()
+  return false
+}
+/********************************
+ * Branch Routes end
+ ********************************/
 
 /**
  * Request Entry Point
  */
 function ksr_request_route() {
-  if (!ksrRouteReqInit()) return
+  if (!routeReqInit()) return
+  if (!routeCancel()) return
 }
