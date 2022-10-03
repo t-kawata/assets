47a74aba73245fb5ed4fb17cf8c6e18322bc16e6
Toshimi Kawata <kawata@shyme.net>
2022-10-03 18:08:53 +0900

    tmp

--- kamailio/dispatcher-for-5.6.x.js
+++ kamailio/dispatcher-for-5.6.x.js
@@ -10,14 +10,25 @@ const notice = function (msg) { KSR.notice(msg) }
 const getPv = function (name) { return KSR.pv.get('$' + name) }
 const setFlag = function (flg) { return KSR.setflag(flg) }
 const slSendReply = function (code, reason) { return KSR.sl.sl_send_reply(code, reason) }
+const sendReply = function (code, reason) { return KSR.sl.send_reply(code, reason) }
 const tCheckTrans = function () { return KSR.tm.t_check_trans() }
 const tPreCheckTrans = function () { return KSR.tmx.t_precheck_trans() }
 const hasToTag = function () { return KSR.siputils.has_totag() }
 const looseRoute = function () { return KSR.rr.loose_route() }
 const isMyselfRuri = function () { return KSR.is_myself_ruri() }
 const isMyselfFuri = function () { return KSR.is_myself_furi() }
+const removeHf = function (headerName) { return KSR.textops.remove_hf(headerName) }
+const recordRoute = function () { return KSR.rr.record_route() }
+const save = function (table, flags) { return KSR.registrar.save(table, flags) }
+const dsSelectDst = function (set, alg) { return KSR.dispatcher.ds_select_dst(set, alg) }
+const dsNextDst = function () { return KSR.dispatcher.ds_next_dst() }
 const tRelay = function () { return KSR.tm.t_relay() }
 const slReplyError = function () { return KSR.sl.sl_reply_error() }
+const tOnFailure = function (failureMethodName) { return KSR.tm.t_on_failure(failureMethodName) }
+const tIsCanceled = function () { return KSR.tm.t_is_canceled() }
+const tCheckStatus = function (replyCode) { return KSR.tm.t_check_status(replyCode) }
+const tBranchTimeout = function () { return KSR.tm.t_branch_timeout() }
+const tBranchReplied = function () { return KSR.tm.t_branch_replied() }
 
 /********************************
  * Branch Routes bgn
@@ -62,14 +73,45 @@ const routeWithinDlg = function () {
   slSendReply(404, 'Not here')
   return false
 }
+const routeInvite = function () {
+  if (KSR.is_INVITE()) setFlag(FLT_ACC)
+  return true
+}
 const routePresence = function () {
-  if (!KSR.is_PUBLISH() && KSR.is_SUBSCRIBE()) return true
+  if (!KSR.is_PUBLISH() && !KSR.is_SUBSCRIBE()) return true
   slSendReply(404, 'Not here')
   return false
+}
+const routeRegister = function () {
+  if (!KSR.is_REGISTER()) return true
+  if (save('location') < 0) slReplyError()
+  if (!routeSelectDst()) return false
+  const reqUri = getPv('ru')
+  const dstUri = getPv('du')
+  info('Request-URI: ' + reqUri)
+  info('Destination-URI: ' + dstUri)
+  return false
+}
+const routeDispatch = function () {
+  if (!routeSelectDst()) return false
+  tOnFailure('failureRouteRtfDispatch')
+  return routeRelay()
+}
+const routeSelectDst = function () {
+  if (dsSelectDst(1, 4) < 0) { sendReply(404, 'No destination'); return false; }
 }
 const routeRelay = function () {
   if (tRelay() < 0) slReplyError()
   return false
+}
+const failureRouteRtfDispatch = function () {
+  if (tIsCanceled() > 0) return
+  if (tCheckStatus('500') || (tBranchTimeout() > 0 && !(tBranchReplied() > 0))) {
+    if (dsNextDst() > 0) {
+      tOnFailure('failureRouteRtfDispatch')
+      return routeRelay()
+    }
+  }
 }
 /********************************
  * Branch Routes end
@@ -83,4 +125,16 @@ function ksr_request_route() {
   if (!routeCancel()) return
   if (!routeAck()) return
   if (!routeWithinDlg()) return
+
+  removeHf('Route')
+  if (KSR.is_INVITE() || KSR.is_SUBSCRIBE()) recordRoute()
+
+  if (!routeInvite()) return
+  if (!routePresence()) return
+  if (!routeRegister()) return
+
+  const usernameInReqURI = getPv('rU')
+  if (usernameInReqURI === null) { slSendReply(484, 'Address Incomplete'); return; }
+
+  if (!routeDispatch()) return
 }
